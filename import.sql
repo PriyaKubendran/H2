6ee427720490058ba90223827c13847ed8172b9a
PriyaKubendran <33031184+PriyaKubendran@users.noreply.github.com>
2019-11-08 09:04:30 -0600

    Add files via upload

---
+++ import.sql
@@ -0,0 +1,5 @@
+insert into snipet values(1, 'This is the first message');
+insert into snipet values(2, 'This is the second message');
+insert into snipet values(3, 'This is the third message');
+insert into snipet values(4, 'This is the fourth message');
+insert into snipet values(5, 'This is the fifth message');
